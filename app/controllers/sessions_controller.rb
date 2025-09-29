51a078590e3307f8dc2d97c7b9bc60de0cd68419
zunerz671 <zunerz671@gmail.com>
2025-09-29 20:08:35 +1000

    sessions controller added

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,13 @@
+class SessionsController < ApplicationController
+  def create
+    user = User.find_by(email: params[:email])
+
+    if user && user.authenticate(params[:password])
+      token = JWT.encode({ user_id: user.id }, Rails.application.credentials.sercret_key_base)
+
+      render json: { jwt: token, user: user }, status: :created
+    else
+      render json: { error: "Invalid email or password" }, status: :unauthorized
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,5 @@ Rails.application.routes.draw do
 
   get "/users" => "users#index"
   post "/signup" => "users#create"
+  post "/login" => "sessions#create"
 end
