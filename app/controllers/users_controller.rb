cc13170b438e9c097d2edc30a51312ab44b5c4e9
zunerz671 <zunerz671@gmail.com>
2025-09-29 19:58:10 +1000

    created user create controller and route

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+  def index
+    user = User.all
+
+    render json: user
+  end
+
+  def create
+    user = User.new(
+      name: params[:name],
+      email: params[:email],
+      password: params[:password],
+      password_confirmation: params[:password_confirmation]
+    )
+
+    if user.save
+      render json: { message: "User created successfully" }, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+end
